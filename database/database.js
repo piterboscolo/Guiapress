d7fa854ca5339beefe437d98ac934b2639d3855b
Murilo <piterboscolo@gmail.com>
2021-12-01 18:00:36 -0300

    Primeiro Commit

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress','victordev','123456abc',{
+const connection = new Sequelize('guiapress','piterboscolo','Clarinh4*',{
     host: 'mysql669.umbler.com',
     dialect: 'mysql',
     timezone: "-03:00"
